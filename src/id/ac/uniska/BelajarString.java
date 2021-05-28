739a364db6b03a1822f68fc02a05581b2de0c0f6
username Github <fauzi3033@gmail.com>
2021-05-28 13:46:06 +0800

    Belajar String dari Array Char

---
+++ src/id/ac/uniska/BelajarString.java
@@ -0,0 +1,37 @@
+package id.ac.uniska;
+
+public class BelajarString {
+    public static void main(String[] args) {
+        char[] uniskaChar = {'U', 'N', 'I', 'S', 'K', 'A'};
+        String uniskaString = new String(uniskaChar);
+        System.out.println(uniskaString);
+
+
+        //#####################################################################
+
+        String namaDepan        = "MUHAMMAD";
+        String namaTengah       = "FAUZI";
+        String namaBelakang     = "CINA";
+
+        String namaLengkap = namaDepan + " " + namaTengah + " " + namaBelakang;
+
+        System.out.println(namaLengkap.length());
+        System.out.println(namaBelakang.indexOf("NIS"));
+        System.out.println(namaLengkap.substring(5));
+        System.out.println(namaLengkap.substring(5, 12));
+        System.out.println(namaLengkap.replace("FAUZI", "SUBHAN"));
+        System.out.println(namaLengkap.toUpperCase());
+        System.out.println(namaLengkap.toLowerCase());
+        System.out.println(namaLengkap.charAt(6));
+
+        String[] namaArray = namaLengkap.split("");
+        System.out.println(namaArray. length);
+        for (String nama : namaArray )
+
+        {
+            System.out.println((nama));
+        }
+
+
+    }
+}
